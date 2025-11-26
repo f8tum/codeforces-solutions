31b4a18270096e7bd084b063c6b8002ae9ad5500
colonelvoid <parthnaik.office@gmail.com>
2025-11-26 18:53:51 +0530

    1901A and header file

---
+++ bits/stdc++.h
@@ -0,0 +1,29 @@
+// Custom bits/stdc++.h for Clang
+// Pulls in most standard C++ headers
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <array>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#include <cfloat>
+#include <cassert>
